e75376c594a8576d98ef5ffeca36e1950c10f77b
NaokiM03 <mattsun.phone@gmail.com>
2019-11-23 18:47:43 +0900

    peek ast

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
+use syn::{ parse_macro_input, ItemImpl };
+
 #[proc_macro_attribute]
 pub fn pub_impl(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let item = dbg!(item);
-    dbg!(_attr);
-    item
+    let ret = item.clone();
+    let ast = parse_macro_input!(item as ItemImpl);
+    dbg!(&ast);
+    dbg!(&ast.items);
+    ret
 }
